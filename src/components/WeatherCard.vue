93615ee484761f73211b62fd158929d17cf3eff3
zinebMachrouh <zinebmac49@gmail.com>
2024-11-12 17:20:25 +0100

    style: enhance WeatherCard layout and add temperature display with unit handling

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -8,11 +8,24 @@
         </div>
         <slot name="settings"></slot>
       </div>
+      <div class="temp">
+        <div class="left">
+          <h2>{{ roundedTemperature }}°</h2>
+          <img :src="'https://openweathermap.org/img/wn/' + apiData.weather[0].icon + '@2x.png'" alt="Weather Icon">
+        </div>
+        <div class="right">
+          <h4>{{ apiData.weather[0].description }}</h4>
+          <p>Feels like {{ roundedFeelsLike }}°</p>
+        </div>
+      </div>
+      <div v-if="allData">
+        <h4>salam</h4>
+      </div>
     </div>
   </template>
   
   <script lang="ts">
-  import { defineComponent, PropType } from 'vue'
+  import { defineComponent, PropType, computed } from 'vue'
   
   export default defineComponent({
     name: 'WeatherCard',
@@ -28,7 +41,38 @@
       formattedTime: {
         type: String,
         required: true
+      },
+      temperatureUnit: {
+        type: String,
+        required: true
+      },
+      allData: {
+        type: Object as PropType<any>,
+        required: false
+      }
+    },
+    setup(props) {
+        const roundedTemperature = computed(() => {
+            const temp = props.temperatureUnit === 'C' ? props.allData.current.temp_c : props.allData.current.temp_f;
+            return temp % 1 >= 0.5 ? Math.ceil(temp) : Math.floor(temp);
+        });
+
+        const roundedFeelsLike = computed(() => {
+            const feelsLike = props.temperatureUnit === 'C' ? props.allData.current.feelslike_c : props.allData.current.feelslike_f;
+            return feelsLike % 1 >= 0.5 ? Math.ceil(feelsLike) : Math.floor(feelsLike);
+        });
+
+      return {
+        roundedTemperature,
+        roundedFeelsLike,
       }
     }
   })
   </script>
+  
+  <style scoped>
+  .right h4 {
+    text-transform: capitalize;
+  }
+  </style>
+  
